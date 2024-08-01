cf3388954e4951f01f47579023b8b4e169112a45
Suhaniaggarwal14 <140490968+Suhaniaggarwal14@users.noreply.github.com>
2024-08-01 06:42:24 +0200

    Add files via upload

---
+++ Day1/src/com/graymatter/Hello.java
@@ -0,0 +1,15 @@
+package com.graymatter;
+
+public class Hello {
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Students...");
+	}
+
+}
+
+
+
